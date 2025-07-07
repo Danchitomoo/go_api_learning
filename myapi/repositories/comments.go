5fdbe9f622d100101c7e1c726a231fb614d01972
Danchitomoo <taka.2000kako@gmail.com>
2025-07-07 17:04:25 +0900

    chap3

---
+++ myapi/repositories/comments.go
@@ -0,0 +1,56 @@
+package repositories
+
+import (
+	"database/sql"
+
+	"github.com/Danchitomoo/go_api_learning/models"
+)
+
+func InsertComment(db *sql.DB, articleID int, comment models.Comment) (models.Comment, error) {
+	const sqlStr = `
+		insert into comments (article_id, message, created_at) values
+		?, ?, now();
+	`
+	var newComment models.Comment
+	result, err := db.Exec(sqlStr, comment.ArticleID, comment.Message)
+	if err != nil {
+		return models.Comment{}, nil
+	}
+	newCommentID, err := result.LastInsertId()
+	if err != nil {
+		return models.Comment{}, nil
+	}
+	newComment.CommentID, newComment.ArticleID, newComment.Message = int(newCommentID), comment.ArticleID, comment.Message
+
+	return newComment, nil
+}
+
+func SelectCommmentList(db *sql.DB, articleID int) ([]models.Comment, error) {
+	const sqlStr = `
+		select * from comments
+		where article_id = ?;
+	`
+	rows, err := db.Query(sqlStr, articleID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	commentArray := make([]models.Comment, 0)
+	for rows.Next() {
+		var comment models.Comment
+		var createdAt sql.NullTime
+		rows.Scan(
+			&comment.CommentID,
+			&comment.ArticleID,
+			&comment.Message,
+			createdAt,
+		)
+		if createdAt.Valid {
+			comment.CreatedAt = createdAt.Time
+		}
+		commentArray = append(commentArray, comment)
+	}
+
+	return commentArray, nil
+}
