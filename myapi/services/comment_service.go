394ce48fb827b9ee61478bef2e54dafea79e4f55
Danchitomoo <taka.2000kako@gmail.com>
2025-07-25 16:27:20 +0900

    Add: handleing the errors

--- myapi/services/comment_service.go
+++ myapi/services/comment_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"github.com/Danchitomoo/go_api_learning/apperrors"
 	"github.com/Danchitomoo/go_api_learning/models"
 	"github.com/Danchitomoo/go_api_learning/repositories"
 )
@@ -8,6 +9,7 @@ import (
 func (s MyAppService) PostCommentService(comment models.Comment) (models.Comment, error) {
 	newComment, err := repositories.InsertComment(s.db, comment)
 	if err != nil {
+		err = apperrors.InsertDataFailed.Wrap(err, "fail to record data")
 		return models.Comment{}, err
 	}
 
