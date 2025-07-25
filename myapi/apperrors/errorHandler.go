ced25ec136f9faa922d1d45f9c89cdc69bd1a5fa
Danchitomoo <taka.2000kako@gmail.com>
2025-07-25 17:56:05 +0900

    fix: errorHanlder

--- myapi/apperrors/errorHandler.go
+++ myapi/apperrors/errorHandler.go
@@ -14,18 +14,17 @@ func ErrorHandler(w http.ResponseWriter, req *http.Request, err error) {
 			Message: "internal process failed",
 			Err:     err,
 		}
-
-		var statusCode int
-		switch appErr.ErrCode {
-		case NAData:
-			statusCode = http.StatusNotFound
-		case NoTargetData, ReqBodyDecodeFailed, BadParam:
-			statusCode = http.StatusBadRequest
-		default:
-			statusCode = http.StatusInternalServerError
-		}
-
-		w.WriteHeader(statusCode)
-		json.NewEncoder(w).Encode(appErr)
 	}
+	var statusCode int
+	switch appErr.ErrCode {
+	case NAData:
+		statusCode = http.StatusNotFound
+	case NoTargetData, ReqBodyDecodeFailed, BadParam:
+		statusCode = http.StatusBadRequest
+	default:
+		statusCode = http.StatusInternalServerError
+	}
+
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(appErr)
 }
